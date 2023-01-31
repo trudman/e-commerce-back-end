b4c113d4bf272b12a74e4bbebfa635e2a7c7f1be
Tyler Rudman <tyler.rudman@gmail.com>
2023-01-31 13:18:01 -0500

    debugged tag model and created product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,41 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { model: Category, attributes: ["id", "category_name"] },
+        { model: Tag, attributes: ["id", "tag_name"] },
+      ],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Category, attributes: ["id", "category_name"] },
+        { model: Tag, attributes: ["id", "tag_name"] },
+      ],
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No Product found with this ID!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
